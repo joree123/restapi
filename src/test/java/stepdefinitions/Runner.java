b7df56d00e7cef88089805076d277f241d4823ac
joree123 <joree123@gmail.com>
2023-10-12 18:26:49 -0700

    2nd commit

--- src/test/java/stepdefinitions/Runner.java
+++ src/test/java/stepdefinitions/Runner.java
@@ -3,7 +3,7 @@ package stepdefinitions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="C:\\Users\\daivi\\eclipse-workspace\\firstRestProject\\src\\test\\java\\featurefiles",
+@CucumberOptions(features="src\\test\\java\\featurefiles",
 glue="stepdefinitions",plugin= {"pretty","json:target/jsonReports/cucumber-report.json"},tags= "@Regression ")
 
 
